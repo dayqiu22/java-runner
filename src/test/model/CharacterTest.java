4ed3b69b51c96b59b3f9fde2aceda7e4dd390a6f
dayqiu19 <dayqiu19@students.ca.ubc.ca>
2023-10-13 14:58:23 -0700

    Removed unused statements from CharacterTest class.

--- src/test/model/CharacterTest.java
+++ src/test/model/CharacterTest.java
@@ -8,22 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CharacterTest {
     private Character testCharacter;
-    private Block testBlock1;
-    private PowerUp testPowerUp1;
-    private Hazard testHazard;
-    private Block testBlock2;
-    private Block testBlock3;
-    private PowerUp testPowerUp2;
 
     @BeforeEach
     void runBefore() {
         testCharacter = new Character(new Position(10, 10));
-        testBlock1 = new Block(new Position(11, 10));
-        testPowerUp1 = new PowerUp(new Position(9, 10), "speedup");
-        testHazard = new Hazard(new Position(13,10));
-        testBlock2 = new Block(new Position(10,11));
-        testBlock3 = new Block(new Position(10,9));
-        testPowerUp2 = new PowerUp(new Position(10, 8), "invulnerable");
     }
 
     @Test
--- src/test/model/GameTest.java
+++ src/test/model/GameTest.java
@@ -1,7 +1,5 @@
 package model;
 
-import model.*;
-import model.Character;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
